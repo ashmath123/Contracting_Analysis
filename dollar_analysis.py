b54c67a47aafbc444c2ec7d5f6a9a7972ae8c567
ashmath123 <122992079+ashmath123@users.noreply.github.com>
2023-04-23 00:14:14 -0400

    Update

--- dollar_analysis.py
+++ dollar_analysis.py
@@ -5,4 +5,12 @@ Created on Sat Apr 22 23:51:22 2023
 
 @author: ashleymathew
 """
+import pandas as pd
+import geopandas as gpd
+import os
+import glob
+
+excel = 'Contracting Dollars by Administration.xlsx'
+contract_dollars = pd.read_excel(excel,sheet_name=None)
+
 
